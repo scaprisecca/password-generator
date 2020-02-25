41205bd592eb201bc4fecfd0a195268bbe48fe06
scaprisecca <50277797+scaprisecca@users.noreply.github.com>
2020-02-25 05:18:48 -0700

    completed javascript file

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ characterAmountContainer = document.querySelector('.characterAmountContainer');
 const includeUppercase = document.getElementById('includeUppercase');
 const includeNumbers = document.getElementById('includeNumbers');
 const includeSymbols = document.getElementById('includeSymbols');
-const btn = document.querySelector('.btn');
+const form = document.querySelector('.form');
 const passwordDisplay = document.querySelector('.password-display');
 
 
 //Adding Event Listeners
 characterAmountContainer.addEventListener('input', snycCharacterAmounts);
 
-btn.addEventListener('submit', generatePassword);
+const lowercaseCharCodes = fromLowToHigh(97, 122);
+const uppercaseCharCodes = fromLowToHigh(65, 90);
+const numberCharCodes = fromLowToHigh(48, 57);
+const symbolCharCodes = fromLowToHigh(33, 47);
 
 
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const characterAmount = characterAmountNumber.value;
+    const uppercaseChecked = includeUppercase.checked;
+    const numbersChecked = includeNumbers.checked;
+    const symbolsChecked = includeSymbols.checked;
+
+    const password = createRandomPassword(characterAmount, uppercaseChecked, numbersChecked, symbolsChecked);
+    passwordDisplay.innerText = password;
+})
 
 //Functions
 function snycCharacterAmounts(e){
@@ -23,52 +37,25 @@ function snycCharacterAmounts(e){
     characterAmountNumber.value = value;
 }
 
-
-function generatePassword(e){
-    e.preventDefault();
-    const characterAmount = characterAmountNumber.value;
-    const uppercaseChecked = includeUppercase.checked;
-    const numbersChecked = includeNumbers.checked;
-    const symbolsChecked = includeSymbols.checked;
-
-    createRandomPassword(characterAmount, uppercaseChecked, numbersChecked, symbolsChecked);
-}
-
 function createRandomPassword(characterAmount, uppercaseChecked, numbersChecked, symbolsChecked) {
-    let password = '';
+    let charCodes = lowercaseCharCodes;
+    if (uppercaseChecked) {charCodes = charCodes.concat(uppercaseCharCodes);}
+    if (numbersChecked) {charCodes = charCodes.concat(numberCharCodes);}
+    if (symbolsChecked) {charCodes = charCodes.concat(symbolCharCodes);}
+
+    let password = [];
     for (let i = 0; i < characterAmount; i++) {
-
+        const characterCodes = charCodes[Math.floor(Math.random() * charCodes.length)];
+        password.push(String.fromCharCode(characterCodes));
     }
+
+    return password.join('');
 }
 
-const uppercaseCharCodes = () => {
-    let uppercaseArray = [];
-    for (let i = 65; i <= 90; i++){
-        uppercaseArray.push(i);
+function fromLowToHigh(low, high) {
+    let array = [];
+    for (let i = low; i <= high; i++) {
+        array.push(i);
     }
-    return uppercaseArray;
-}
-
-const numberCharCodes = () => {
-    let numberArray = [];
-    for (let i = 48; i <= 57; i++){
-        numberArray.push(i);
-    }
-    return numberArray;
-}
-
-const symbolCharCodes = () => {
-    let symbolArray = [];
-    for (let i = 33; i <= 47; i++){
-        symbolArray.push(i);
-    }
-    return symbolArray;
-}
-
-const lowercaseCharCodes = () => {
-    let lowercaseArray = [];
-    for (let i = 97; i <= 122; i++) {
-        lowercaseArray.push(i);
-    }
-    return lowercaseArray;
+    return array;
 }
